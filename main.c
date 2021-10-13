85f4c47cd84011f39f5042e4b8e69cb4be82001f
awesome-sse <sse103@mail.ru>
2021-10-13 20:20:09 +0300

    Add run()

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ void print_team_table(team_list * t_list) {
 }
 
 void print_top_teams(team_list * t_list) {
+    printf("\tTOP LIST\n");
     team_list toplist = create_top_list(t_list);
     print_team_table(&toplist);
     clear_team_list(&toplist);
@@ -33,26 +34,21 @@ void add_team(team_list * t_list) {
     add_team_in_list(t_list, number, name, time, items);
 }
 
-int processor_of_command(int cmd, team_list * t_list) {
-    if (cmd == 1) {
-        add_team(t_list);
-    }
-    else if (cmd == 2) {
-        print_top_teams(t_list);
-    }
-    else return 0;
-
-    return 1;
+void processor_of_command(int cmd, team_list * t_list) {
+    if (cmd == 1) add_team(t_list);
+    else if (cmd == 2) print_team_table(t_list);
+    else if (cmd == 3) print_top_teams(t_list);
+    else if (cmd != 0) printf("Command is not recognized\n");
 }
 
 int run() {
     team_list t_list = create_team_list();
+    printf("1 - Add team, 2 - Print list of all teams, 3 - Print TOP list of teams, 0 - exit\n");
     int command = 1;
-    printf("1 - Add team, 2 - Print top list of teams, Another command - exit\n");
     while (command) {
         printf("Input command: ");
         scanf("%i", &command);
-        command = processor_of_command(command, &t_list);
+        processor_of_command(command, &t_list);
     }
     clear_team_list(&t_list);
 }
